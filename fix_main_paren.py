86803277ff3eb4983d1e78f1014954ca53f01ea0
Bath4856 <theophile.bakang@gmail.com>
2026-05-26 20:54:52 +0200

    chore: suppression scripts temporaires de patch racine repo

--- fix_main_paren.py
+++
@@ -1,7 +0,0 @@
-﻿content = open(r"G:\osa-observatory\api\main.py", encoding="utf-8").read()
-content = content.replace(
-    "# SPRINT14 DEPRECATED --     ew_router as ew_sprint7_phase2_router, scenario_router\n)",
-    "# SPRINT14 DEPRECATED --     ew_router as ew_sprint7_phase2_router, scenario_router\n# SPRINT14 DEPRECATED -- )"
-)
-open(r"G:\osa-observatory\api\main.py", "w", encoding="utf-8").write(content)
-print("OK")
--- patch_main_eparticipation.py
+++
@@ -1,9 +0,0 @@
-﻿content = open(r"G:\osa-observatory\api\main.py", encoding="utf-8").read()
-old = "from api.routers.opendata import router as opendata_router"
-new = "from api.routers.opendata import router as opendata_router\nfrom api.routers.eparticipation import router as eparticipation_router"
-content = content.replace(old, new, 1)
-old2 = "app.include_router(opendata_router)"
-new2 = "app.include_router(opendata_router)\napp.include_router(eparticipation_router)"
-content = content.replace(old2, new2, 1)
-open(r"G:\osa-observatory\api\main.py", "w", encoding="utf-8").write(content)
-print("OK")
